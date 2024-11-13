781869d205f2fa88a1b8a7561bf85ed7c6f31872
ImIshan7 <ishandanu999@gmail.com>
2024-11-13 19:55:56 +0530

    Add File Type

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust the path as necessary
+    '././view/custom.css'   // Include the path to your custom CSS file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
