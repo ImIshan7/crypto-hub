fbb0d8c13e7bdc63fdfa7aa2db4f706d51dc5c58
ImIshan7 <ishandanu999@gmail.com>
2024-11-16 20:30:18 +0530

    Layout Content Updated

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+    return (
+        <div className="min-h-screen bg-black">
+            <Navbar />
+            <main className="pt-16">{children}</main>
+            <Footer />
+        </div>
+    );
+};
+
+export default Layout;
